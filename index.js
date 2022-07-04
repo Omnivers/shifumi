0e67665b5edc0be21c5c0a146f70e7d8e6c7ddd0
Omnivers <Twersimor.stock@outlook.com>
2022-07-04 13:56:55 +0200

    Setting the files, HTML/CSS

---
+++ index.js
@@ -0,0 +1,6 @@
+let images=["./img/1.png","./img/2.png","./img/3.png"];
+const imgPlayer=document.getElementById("imagePlayer");
+const imgAI=document.getElementById("imageIA");
+
+// imgPlayer.src=images[1]
+
