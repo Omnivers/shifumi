8fbb755f8903e1f62d50cdf076f0a61f7b1e099b
Omnivers <Twersimor.stock@outlook.com>
2022-07-05 14:34:43 +0200

    Change of algo/Css/HTML

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-let images=["./img/1.png","./img/2.png","./img/3.png"];
-const imgPlayer=document.getElementById("imagePlayer");
-const imgAI=document.getElementById("imageIA");
-const gameEnd=document.getElementById("gameEnd");
+let images=["./img/1.png","./img/2.png","./img/3.png"]; //The imgs of rock/paper/scissors
+const imgPlayer=document.getElementById("imagePlayer"); // imgPlayer equals the the img that the player choosed
+const imgAI=document.getElementById("imageIA");//imgAI equals the img that IA choosed
+const gameEnd=document.getElementById("gameEnd");// For the results animation
+const scoregeneral=document.querySelector('.score');
+// class Round{
+//     constructor(round,resultplayer,resultIA,result){
+//         this.round=round;
+//         this.resultplayer=resultplayer;
+//         this.resultIA=resultIA;
+//         this.result=result;
+//     }
 
+
+// }
+// firstable we declare string variables of what the player&IA choosed with a null value
 let choiceplayer;
 let choiceAI;
-
+// Then we declare the points and the rounds that the player&AI earned by playing
 let scorePl=0;
 let scoreIA=0;
-
+let roundAI=0;
+let roundPl=0;
+//DOM setting for the rock/paper/scissors :
 const pierre=document.getElementById("pierre");
 const papier=document.getElementById("papier");
 const ciseaux=document.getElementById("ciseaux");
 
 pierre.addEventListener("click", function() {
-     imgAI.src="./img/giphy.gif";
+     imgAI.src="./img/giphy.gif";//Animation before the AI choose his random img
     setTimeout(rock,1000)
     });
 papier.addEventListener("click",function() {
@@ -26,24 +39,22 @@ ciseaux.addEventListener("click",function() {
    setTimeout(cut,1000)
    });
 
-
-
 // game functions instructions >>>>
 function rock(){ 
-    papier.style.backgroundColor='black';
-    ciseaux.style.backgroundColor='black';
+    papier.style.backgroundColor='blueviolet';
+    ciseaux.style.backgroundColor='blueviolet';
     pierre.style.backgroundColor='green';
-    document.getElementById("Random").style.backgroundColor='green';
-    randomInteger();
+    document.getElementById("Random").style.backgroundColor='green';//style to show out what the player clicked
+    randomInteger();//generate the random number for our AI
     imgPlayer.src=images[0];
     choiceplayer='Pierre';
-    imgAI.src=images[randomnum];
-    choice();
-    setTimeout(score,100);
+    imgAI.src=images[randomnum];//generate an img for the AI
+    choice();//nominate the choice of AI as a string
+    setTimeout(score,100);//launch the result of the round
 }
 function paper(){
-    ciseaux.style.backgroundColor='black';
-    pierre.style.backgroundColor='black';
+    ciseaux.style.backgroundColor='blueviolet';
+    pierre.style.backgroundColor='blueviolet';
     papier.style.backgroundColor='green';
     document.getElementById("Random").style.backgroundColor='green';
     randomInteger()
@@ -54,8 +65,8 @@ function paper(){
     setTimeout(score,100);
 }
 function cut(){
-    papier.style.backgroundColor='black';
-    pierre.style.backgroundColor='black';
+    papier.style.backgroundColor='blueviolet';
+    pierre.style.backgroundColor='blueviolet';
     ciseaux.style.backgroundColor='green';
     document.getElementById("Random").style.backgroundColor='green';
     randomInteger();
@@ -66,18 +77,17 @@ function cut(){
     setTimeout(score,100);
 }
 function score(){
-    if(imgPlayer.src===imgAI.src){
-        alert("Egalité");
-    }
-    else if(choiceplayer==='Pierre' && choiceAI==='Papier'){
+    if(choiceplayer==='Pierre' && choiceAI==='Papier'){
         pierre.style.backgroundColor='red';
         scoreIA++;
     }
     else if(choiceplayer==='Pierre' && choiceAI==='Ciseaux'){
         scorePl++;
+        document.getElementById("Random").style.backgroundColor='red';
     }
     else if(choiceplayer==='Papier' && choiceAI==='Pierre'){
         scorePl++;
+        document.getElementById("Random").style.backgroundColor='red';
     }
     else if(choiceplayer==='Papier' && choiceAI==='Ciseaux'){
         papier.style.backgroundColor='red';
@@ -89,27 +99,39 @@ function score(){
     }
     else if(choiceplayer==='Ciseaux' && choiceAI==='Papier'){
         scorePl++;
+        document.getElementById("Random").style.backgroundColor='red';
     }
     document.getElementById('scorePlayer').innerHTML=scorePl
     document.getElementById('scoreIA').innerHTML=scoreIA
     document.getElementById("Random").innerHTML=choiceAI
-    if (scorePl===5) {
+    if(scorePl>2){
+        roundPl++
         scorePl=0;
         scoreIA=0;
-        const video=document.createElement('video');
-        video.src='./img/win.mp4';
-        video.autoplay = true;
-        video.controls = false;
-        video.muted = false;
-        gameEnd.appendChild(video);
-        gameEnd.style.display="block"
-        video.addEventListener('ended', (event) => {
-            gameEnd.style.display = 'none';
-            gameEnd.innerHTML='';
-        })
-
+        leaderBoard();
+        document.getElementById('scorePlayer').innerHTML=scorePl
+        document.getElementById('scoreIA').innerHTML=scoreIA
     }
-    else if(scoreIA===5){
+    else if(scoreIA>2){
+        roundAI++
+        scoreIA=0;
+        scorePl=0;
+        leaderBoard();
+        document.getElementById('scorePlayer').innerHTML=scorePl
+        document.getElementById('scoreIA').innerHTML=scoreIA
+    }
+    else if (roundPl>4) {
+        scoregeneral.innerHTML=``;
+    }
+    else if(roundAI>4){
+        scoregeneral.innerHTML=``;
+    }
+}
+function leaderBoard(){
+    scoregeneral.innerHTML=`L'IA a remporté :${roundAI} rounds<br>
+    Vous avez remporté : ${roundPl} rounds
+    `;
+    if(roundAI>4){
         scoreIA=0;
         scorePl=0;
         const video=document.createElement('video');
@@ -123,10 +145,29 @@ function score(){
             gameEnd.style.display = 'none';
             gameEnd.innerHTML='';
         })
-
+        scoregeneral.innerHTML=`GAME OVER !`;
+        roundAI=0;
+        roundPl=0;
+    }
+    else if(roundPl>4){
+        scorePl=0;
+        scoreIA=0;
+        const video=document.createElement('video');
+        video.src='./img/win.mp4';
+        video.autoplay = true;
+        video.controls = false;
+        video.muted = false;
+        gameEnd.appendChild(video);
+        gameEnd.style.display="block"
+        video.addEventListener('ended', (event) => {
+            gameEnd.style.display = 'none';
+            gameEnd.innerHTML='';
+        })
+        scoregeneral.innerHTML=`CONGRATULATION !`;
+        roundAI=0;
+        roundPl=0;
     }
 }
-
 
 
 // Randomnum generator for the AI select >>
